39457a63d83b55f78ef75bcaa6039dfefe27174e
Thiago <thiagodeboliveira@outlook.com>
2024-11-11 15:05:56 -0300

    Bug chato aaaaaaaaaa

--- src/main/java/com/estudosjava/dreamgames/DreamgamesApplication.java
+++ src/main/java/com/estudosjava/dreamgames/DreamgamesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DreamgamesApplication {
+public class  DreamgamesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamgamesApplication.class, args);
