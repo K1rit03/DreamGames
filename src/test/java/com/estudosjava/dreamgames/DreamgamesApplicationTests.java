8889eed538ea9bdd7d26e5c5a841fc84bdff8a28
Thiago <thiagodeboliveira@outlook.com>
2024-11-11 11:39:04 -0300

    Salvando projeto spring no repósitorio

---
+++ src/test/java/com/estudosjava/dreamgames/DreamgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudosjava.dreamgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
