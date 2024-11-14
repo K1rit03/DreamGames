76c05e2362143a1860d115774560066faa5a8c55
Thiago <thiagodeboliveira@outlook.com>
2024-11-14 10:26:03 -0300

    Getting games by ID

--- src/main/java/com/estudosjava/dreamgames/controllers/GameController.java
+++ src/main/java/com/estudosjava/dreamgames/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.estudosjava.dreamgames.controllers;
 
+import com.estudosjava.dreamgames.dto.GameDTO;
 import com.estudosjava.dreamgames.dto.GameMinDto;
 import com.estudosjava.dreamgames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     // Adicione a anotação @GetMapping para que o método seja um endpoint de GET
     @GetMapping
--- src/main/java/com/estudosjava/dreamgames/services/GameService.java
+++ src/main/java/com/estudosjava/dreamgames/services/GameService.java
@@ -1,13 +1,17 @@
 package com.estudosjava.dreamgames.services;
 
 
+import com.estudosjava.dreamgames.dto.GameDTO;
 import com.estudosjava.dreamgames.dto.GameMinDto;
 import com.estudosjava.dreamgames.entities.Game;
 import com.estudosjava.dreamgames.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -15,7 +19,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return  new GameDTO(result);
 
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDto(x)).toList();
